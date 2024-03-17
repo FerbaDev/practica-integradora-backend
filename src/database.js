4a331821bd2b57f7ed806debbda379a6c62a8954
FerbaDev <elferbarron1@gmail.com>
2024-03-16 23:46:07 -0300

    se trabaja en database.js

---
+++ src/database.js
@@ -0,0 +1,7 @@
+//configuramos mongo db
+const mongoose = require("mongoose");
+ //crear la conexion con la base de datos
+
+mongoose.connect(`mongodb+srv://Ferbadev:${process.env.PASSWORD}@cluster0.qaz6nck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => console.log("Conexion exitosa"))
+    .catch((error) => console.log("Conexion falló", error))
